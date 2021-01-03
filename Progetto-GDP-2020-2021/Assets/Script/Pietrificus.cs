aa031449003064ce8908b8b1207a642524ab12ce
Ste_Dambra <stefano.dambra@studenti.unimi.it>
2021-01-03 18:44:56 +0100

    winner winner chicken dinner

---
+++ Progetto-GDP-2020-2021/Assets/Script/Pietrificus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pietrificus : MonoBehaviour
+{
+
+    private bool castSpell = false;
+
+    public bool CastSpell
+    {
+        set => castSpell = value;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (castSpell && other.CompareTag("Box"))
+        {
+            other.GetComponent<Animator>().speed = 0;
+        }
+    }
+}
